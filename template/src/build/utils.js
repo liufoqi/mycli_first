8e874955a37be83746e46c399e3c99dcec7e308a
liuufo <1139329576@qq.com>
2021-04-20 11:28:27 +0800

    修改模板

--- template/src/build/utils.js
+++
@@ -1,7 +0,0 @@
-const path =require('path')
-exports.resolve=function(filePath){
-    return path.join(__dirname,'./../',filePath)
-}
-exports.resolveFile=function (dir){
-    return path.join(dir)
-}
